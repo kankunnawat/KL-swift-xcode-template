4c745156ee7089d4526e88db2efcbd7bc4d7ca22
kankunnawat <kan1657@gmail.com>
2024-07-04 15:22:00 +0700

    Update template: Remove public/open modifier

--- KL Swift/Scene.xctemplate/___FILEBASENAME___ViewController.swift
+++ KL Swift/Scene.xctemplate/___FILEBASENAME___ViewController.swift
@@ -2,29 +2,29 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-open class ___VARIABLE_sceneName___ViewController: BaseViewController {
+class ___VARIABLE_sceneName___ViewController: BaseViewController {
     let viewModel: ___VARIABLE_sceneName___ViewModel
 
     // MARK: - Life Cycles
-    public init(viewModel: ___VARIABLE_sceneName___ViewModel) {
+    init(viewModel: ___VARIABLE_sceneName___ViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
     }
 
-    public required init?(coder: NSCoder) {
+    required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
     private let mainView = ___VARIABLE_sceneName___View()
 
-    open override func viewDidLoad() {
+    override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationBar()
         setupView()
         bindViewModel()
     }
 
-    open override func viewWillAppear(_ animated: Bool) {
+    override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         baseNavigationController?.setNavigationBarHidden(false, animated: true)
     }
--- KL Swift/Scene.xctemplate/___FILEBASENAME___ViewModel.swift
+++ KL Swift/Scene.xctemplate/___FILEBASENAME___ViewModel.swift
@@ -1,16 +1,16 @@
 import RxSwift
 import RxCocoa
 
-open class ___VARIABLE_sceneName___ViewModel: ViewModelType {
+class ___VARIABLE_sceneName___ViewModel: ViewModelType {
     private let router: ___VARIABLE_sceneName___Router
     private let useCase: <#placeholder#>
 
-    public init(router: ___VARIABLE_sceneName___Router, useCase: <#placeholder#>, ) {
+    init(router: ___VARIABLE_sceneName___Router, useCase: <#placeholder#>, ) {
         self.router = router
         self.useCase = useCase
     }
 
-    public struct Input {
+    struct Input {
 
     }
 
