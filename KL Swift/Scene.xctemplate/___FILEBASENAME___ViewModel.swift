9471f2b095882b3e756ffead8959ec76a75348cc
kankunnawat <kan1657@gmail.com>
2024-07-13 17:04:35 +0700

    Update template

--- KL Swift/Scene.xctemplate/___FILEBASENAME___ViewModel.swift
+++ KL Swift/Scene.xctemplate/___FILEBASENAME___ViewModel.swift
@@ -7,7 +7,7 @@ class ___VARIABLE_sceneName___ViewModel: ViewModelType {
     private let router: ___VARIABLE_sceneName___Router
     private let useCase: <#placeholder#>
 
-    init(router: ___VARIABLE_sceneName___Router, useCase: <#placeholder#>, ) {
+    init(router: ___VARIABLE_sceneName___Router, useCase: <#placeholder#>) {
         self.router = router
         self.useCase = useCase
     }
